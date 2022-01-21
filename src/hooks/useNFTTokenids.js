fc74d1cef375fa8a4eda4a3cfe23304aca17cf92
MontyWilliams <3650@holbertonschool.com>
2022-01-21 07:35:03 -0600

    fixed bug from tutorial and can now load nfts from any collection on any chain

---
+++ src/hooks/useNFTTokenids.js
@@ -0,0 +1,34 @@
+import { useMoralisDapp } from "providers/MoralisDappProvider/MoralisDappProvider";
+import { useEffect, useState } from "react";
+import { useMoralisWeb3Api, useMoralisWeb3ApiCall } from "react-moralis";
+import { useIPFS } from "./useIPFS";
+
+export const useNFTTokenids = () => {
+  const { token } = useMoralisWeb3Api();
+  const { chainId } = useMoralisDapp();
+  const { resolveLink } = useIPFS();
+  const [NFTTokenids, setNFTTokenids] = useState([]);
+  const {
+    fetch: getNFTTokenids,
+    data,
+    error,
+    isLoading,
+  } = useMoralisWeb3ApiCall(token.getAllTokenIds, { chain: chainId, address: '0x29F6D2381f82E6d52ad54f403DAff421Fdb3BdA2'  });
+
+  useEffect(() => {
+    if (data?.result) {
+      const NFTs = data.result;
+      for (let NFT of NFTs) {
+        if (NFT?.metadata) {
+          NFT.metadata = JSON.parse(NFT.metadata);
+          // metadata is a string type
+          NFT.image = resolveLink(NFT.metadata?.image);
+        }
+      }
+      setNFTTokenids(NFTs);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+
+  return { getNFTTokenids, NFTTokenids, error, isLoading };
+};
